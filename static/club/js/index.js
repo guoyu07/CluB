ed143d12768e5ff06453d32defe43522397e6f43
QC-Li <cc794058798@sina.cn>
2015-10-29 03:15:18 +0800

    Initial commit(just has some html/css/js code)

---
+++ static/club/js/index.js
@@ -0,0 +1,8 @@
+/**
+ * Created by albert on 15-10-29.
+ */
+
+$(function(){
+//TODO:添加登陆按钮效果，还有在css文件添加表单样式，参考Bao和python/jquery/...的
+
+})
---
+++ static/club/js/jquery.lqc.extension.js
@@ -0,0 +1,22 @@
+/**
+ * Created by albert on 15-10-29.
+ */
+/*类似于继承了jquery扩展jquery
+
+函数(全局变量不介绍)      功能
+
+
+
+
+
+*/
+(function($){
+    //定义全局变量
+    //config
+    //每次加载帖子个数
+    $.LQC_POST_LOAD_COUNT = 5;
+    //每次加载评论数目
+    $.LQC_COMMENT_LOAD_COUNT = 3;
+
+})
+
